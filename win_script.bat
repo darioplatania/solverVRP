e5623336ade9b68b42a0520176cd30cdd184facf
dario platania <dariopl91@gmail.com>
2016-01-03 16:28:48 +0100

    fix win_script

--- win_script.bat
+++ win_script.bat
@@ -9,7 +9,10 @@ echo  CREATE solutions.csv file
 set MYPROG=java -jar solverVRP.jar
 set num_veicoli=0
 
-#lasciare files_1.txt, ma se non funziona provare se non funziona con files.txt
+echo -n "Enter the first number --> "
+read num_veicoli
+
+#lasciare files_1.txt, ma se non funziona provare con files.txt
 for /F %%i in (files_1.txt) do (
 	echo %MYPROG% -i %%i
 	%MYPROG% -i %%i -n %num_veicoli%
